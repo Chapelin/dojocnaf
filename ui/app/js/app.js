beab32358fddf7e401d1454537c667fddbb286d7
cafequipe4 <cafdojoequipe4@outlook.fr>
2017-06-30 12:28:41 +0200

    DOJO !

--- ui/app/js/app.js
+++ ui/app/js/app.js
@@ -17,10 +17,12 @@
     'use strict';
 
     angular.module('appGeoCaf', [
+        "ngRoute",
         "ngSanitize",
         "ngProgress",
         "ngResource",
-        "ui.bootstrap"
+        "ui.bootstrap",
+        'ui.router'
     ]).constant('geoCafConfig', {
         "version": "0.0.1",
         "apis": {
@@ -29,30 +31,100 @@
         }
     });
 
+    angular.module('appGeoCaf').config(appGeoCafConfiguration);
+
+    //appGeoCafConfiguration.$inject = ['$stateProvider', '$urlRouterProvider', '$locationProvider', '$httpProvider', '$routeProvider', 'geoCafConfig'];
+    //function appGeoCafConfiguration($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider, $routeProvider, geoCafConfig) {
+    //    // MVC routing patterns
+    //    $urlRouterProvider.otherwise('/');	// default route
+    //    $stateProvider
+    //        /******************************************************************/
+    //        .state('app', {
+    //            url: '/',
+    //            templateUrl: 'index.html',
+    //            controller: 'MainController'
+    //        })
+    //        /******************************************************************/
+    //        .state('contact', {
+    //            url: '/contact',
+    //            views: {
+    //                'content': {
+    //                    templateUrl: 'views/contact.html',
+    //                    controller: 'MainController'
+    //                }
+    //            },
+    //            parent: 'app'
+    //        })
+    //        /******************************************************************/
+    //        .state('carto', {
+    //            url: '/carto',
+    //            views: {
+    //                'content': {
+    //                    templateUrl: 'views/carto.html',
+    //                    controller: 'MainController'
+    //                }
+    //            },
+    //            parent: 'app'
+    //        })
+    //        /******************************************************************/
+    //        .state('demarche', {
+    //            url: '/demarche',
+    //            views: {
+    //                'content': {
+    //                    templateUrl: 'views/demarche.html',
+    //                    controller: 'MainController'
+    //                }
+    //            },
+    //            parent: 'app'
+    //        });
+    //    $locationProvider.hashPrefix('!');
+
+    //    $routeProvider
+    //        //.when('/', {
+    //        //    templateUrl: 'index.html'
+    //        //})
+    //        .when('/carto', {
+    //            templateUrl: 'carto.html',
+    //            controller: 'MainCtrl'
+    //        })
+    //        .when('/demarche', {
+    //            templateUrl: 'views/demarche.html',
+    //            controller: 'MainCtrl'
+    //        })
+    //        .otherwise({
+    //            redirectTo: '/'
+    //        });
+    //}
+
+    appGeoCafConfiguration.$inject = ['$locationProvider', '$routeProvider'];
+    function appGeoCafConfiguration($locationProvider, $routeProvider) {
+        $routeProvider
+            .when('/carto', {
+                templateUrl: 'views/carto.html',
+                controller: 'MainCtrl'
+            })
+            .when('/demarche', {
+                templateUrl: 'views/demarche.html',
+                controller: 'DemarcheCtrl'
+            })
+            .when('/contact', {
+                templateUrl: 'views/contact.html',
+                controller: 'ContactCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    }
+
     angular.module('appGeoCaf').run(appGeoCafRun);
 
     appGeoCafRun.$inject = ['$rootScope', 'ngProgressFactory'];
     function appGeoCafRun($rootScope, ngProgressFactory) {
-        $rootScope.alerts = [];
-        $rootScope.user = null;
-
         $rootScope.lang = 'fr';
 
         $rootScope.ngProgress = ngProgressFactory.createInstance();
 
         $rootScope.ngProgress.setColor('#000');
         $rootScope.ngProgress.setHeight('3px');
-        
-        $rootScope.closeAlert = function (index) {
-            $rootScope.alerts.splice(index, 1);
-        };
-
-        $rootScope.closeError = function () {
-            delete $rootScope.error;
-        };
-
-        $rootScope.closeSuccess = function () {
-            delete $rootScope.success;
-        };
     }
 })();
--- ui/app/js/controllers/mainCtrl.js
+++ ui/app/js/controllers/mainCtrl.js
@@ -11,14 +11,7 @@
         $scope.loading = { value: false };
         $scope.filtres = {};
         $scope.display = {
-            legend: false,
-            polpo: false,
-            cvip: false,
-            cvi: false,
-            acces: false,
-            lvs: false,
-            bon_plan: false,
-            lien_transac: false
+            legend: false
         };
         $scope.markers = [];
         $scope.alerts = [];
@@ -30,12 +23,12 @@
         var markers = {
             agence: { color: 'blue', icon: 'building' },
             antenne: { color: 'brown', icon: 'wifi' },
-            centre_impots: { color: 'magenta', icon: 'money' },
-            cpam: { color: 'cyan', icon: 'cab' },
+            centre_des_impots: { color: 'magenta', icon: 'money' },
+            //cpam: { color: 'cyan', icon: 'cab' },
             mairie: { color: 'purple', icon: 'institution' },
-            permanence: { color: 'yellow', icon: 'id-card' },
-            pole_emploi: { color: 'green', icon: 'user-plus' },
-            allocataire: { color: 'red', icon: 'user' }
+            permanence: { color: 'orange', icon: 'id-card' },
+            //pole_emploi: { color: 'green', icon: 'user-plus' },
+            allocataire: { color: 'green', icon: 'user' }
         }
 
         // Paramétrage et définition de la map Leaflet
@@ -62,11 +55,11 @@
             AppBanServices.locateAllocataire({ address: _adresse }, function (results) {
                 console.log(results);
                 var _tmp_data = {
-                    adresse: results.features[0].properties.label,
-                    lat: results.features[0].geometry.coordinates[1],
-                    lon: results.features[0].geometry.coordinates[0]
+                    AdresseComplete: results.features[0].properties.label,
+                    X: results.features[0].geometry.coordinates[1],
+                    Y: results.features[0].geometry.coordinates[0]
                 }
-                load_marker(_tmp_data, markers.allocataire);
+                load_marker(_tmp_data, 'allocataire');
                 $rootScope.ngProgress.complete();
             }, function (error) {
                 $rootScope.ngProgress.reset();
@@ -75,49 +68,98 @@
         }
 
         var load_marker = function (result, marker_style) {
-            if (marker_style == markers.antenne) {
-                var content = '<div style="width:230px">' +
-                    '<table class="table table-striped table-bordered table-condensed">' +
-                    '<tr><th>Adresse</th><td><b>' + result.adresse + '</b></td></tr>' +
-                    '</table>' +
-                    '<div style="clear:both"></div>' +
-                    '</div>';
+            var content = null;
+            if (marker_style != 'allocataire') {
+                content = '<table>' +
+                    '			<thead>' +
+                    '				<tr>' +
+                    '					<th colspan=2 style="white-space:nowrap;"><h2>' + result.Nom + '</h2></th>' +
+                    '				</tr>' +
+                    '			</thead>' +
+                    '			<tbody>' +
+                    '				<tr>' +
+                    '					<td>Type d\'accueil</td><td>' + result.Type_Libelle + '</td> <td class=""></td>' +
+                    '				</tr>' +
+                    '				<tr>' +
+                    '					<td>Adresse</td><td  class="">' + result.Adresse + '</td><td class=""></td>' +
+                    '				</tr>' +
+                    '					<tr>' +
+                    '					<td>Ville</td><td  class="">' + result.Ville + ' ' + result.CodePostal + '</td>' +
+                    '				</tr>' +
+                    '				<thead>' +
+                    '					<tr>' +
+                    '						<th colspan=2>MODALITE d\'ACCUEIL</th > ' +
+                    '					</tr>' +
+                    '				</thead>' +
+                    '				<tr>' +
+                    '					<td class="">Jours d\'ouverture</td > <td class="">Lun - Mar - Mer - Jeu - Ven</td>' +
+                    '				</tr>' +
+                    '				<tr>' +
+                    '					<td class="">Heures d\'ouverture</td > <td class="">8h30 - 16h00</td> <td class=""></td>' +
+                    '				</tr>' +
+                    '				<tr>' +
+                    '					<td class="">Acces internet</td><td  class="">OUI</td><td  class=""></td>' +
+                    '				</tr>' +
+                    '				<tr>' +
+                    '					<td class="">Accompagnement Web</td><td  class="">OUI</td>' +
+                    '				</tr>' +
+                    '				<tr>' +
+                    '					<td class="">Borne interactive</td><td  class="">NON</td>' +
+                    '				</tr>' +
+                    '				<tr>' +
+                    '					<td class="">Impression - scan</td><td  class="">OUI</td>' +
+                    '				</tr>' +
+                    '				<thead>' +
+                    '					<tr>' +
+                    '						<th colspan=2>AFFLUENCE</th>' +
+                    '					</tr>' +
+                    '				</thead>' +
+                    '				<tr>' +
+                    '					<td colspan=2 ><img class="aff" src="img/affluence.png" alt="plage affluence" width="350" /></td>' +
+                    '				</tr>	' +
+                    '			</tbody>' +
+                    '		</table>';
+            } else {
+                content = '<b style="white-space:nowrap;">Vous êtes ici</b>';
             }
 
             var customMarker = null;
 
-            if (marker_style == markers.pole_emploi) {
+            if (marker_style == 'pole_emploi') {
                 customMarker = L.icon({
                     iconUrl: 'img/pole_emploi.png'
                 });
-            } else if (marker_style == markers.cpam) {
+            } else if (marker_style == 'cpam') {
                 customMarker = L.icon({
                     iconUrl: 'img/cpam.png'
                 });
             } else {
                 customMarker = L.AwesomeMarkers.icon({
-                    icon: marker_style.icon,
-                    markerColor: marker_style.color,
-                    prefix: 'glyphicon',
+                    icon: markers[marker_style].icon,
+                    markerColor: markers[marker_style].color,
+                    prefix: 'fa',
                     iconColor: 'white'
                 });
             }
 
-            var marker = L.marker([result.lat, result.lon], {
-                icon: customMarker,
-                zIndexOffset: 500
-            }).addTo(map);
+            if (result.X && result.Y) {
+                var marker = L.marker([result.X, result.Y], {
+                    icon: customMarker,
+                    zIndexOffset: (marker_style != 'allocataire') ? 500 : 700
+                }).addTo(map);
 
-            var linkFunction = $compile(angular.element(content)),
-                newScope = $scope.$new();
+                var linkFunction = $compile(angular.element(content)),
+                    newScope = $scope.$new();
 
-            marker.bindPopup(linkFunction(newScope)[0], {
-                maxWidth: "auto",
-                closeButton: true
-            });
+                var markerPopup = marker.bindPopup(linkFunction(newScope)[0], {
+                    maxWidth: "auto",
+                    closeButton: true
+                });
 
-            if (marker_style == markers.allocataire) {
-                map.setView([result.lat, result.lon]);
+                if (marker_style == 'allocataire') {
+                    map.setView([result.X, result.Y]);
+                    markerPopup.openPopup();
+                }
             }
         }
 
@@ -136,14 +178,8 @@
         var getDataPOI = function () {
             AppCafServices.getPoi({}, function (results) {
                 console.log("getDataPOI:", results);
-                // TODO : add markers
                 angular.forEach(results, function (result, key) {
-                    var _tmp_data = {
-                        adresse: result.AdresseComplete,
-                        lat: result.X,
-                        lon: result.Y
-                    }
-                    load_marker(_tmp_data, result.Type);
+                    load_marker(result, result.Type);
                 });
                 
                 $rootScope.ngProgress.complete();
